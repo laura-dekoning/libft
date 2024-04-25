1d639fe17464a564e8ee43801220cf9fa126c4f7
Laura de Koning <lade-kon@student.codam.nl>
2024-04-25 17:44:32 +0200

    Added ft_strcmp

---
+++ src/libft/string/ft_strcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strcmp.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/04/25 17:43:40 by lade-kon      #+#    #+#                 */
+/*   Updated: 2024/04/25 17:43:48 by lade-kon      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *str1, const char *str2)
+{
+	size_t	i;
+
+	i = 0;
+	while (str1[i] != '\0' || str2[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+	}
+	return (0);
+}
