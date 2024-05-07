70f6cdff731726ef6765f6337835f9e7c6fe5c13
Laura de Koning <lade-kon@student.codam.nl>
2024-05-07 19:28:36 +0200

    Fixed strcmp

--- src/libft/string/ft_strcmp.c
+++ src/libft/string/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/25 17:43:40 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/05/07 19:24:06 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/05/07 19:27:50 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strcmp(const char *str1, const char *str2)
 	size_t	i;
 
 	i = 0;
-	if (ft_strlen(str1) == ft_strlen(str2))
+	if (ft_strlen(str1) != ft_strlen(str2))
 		return (EXIT_FAILURE);
 	while (str1[i] != '\0' || str2[i] != '\0')
 	{
