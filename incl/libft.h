48999c651e9d06629da6d65c63c10a5ca16f7651
Laura de Koning <lade-kon@student.codam.nl>
2024-09-05 15:56:10 +0200

    made ft_free_array with char ** again

--- incl/libft.h
+++ incl/libft.h
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/03 07:39:09 by lde-koni      #+#    #+#                 */
-/*   Updated: 2024/08/08 16:41:33 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/09/05 15:10:44 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,7 +104,7 @@ void		*ft_memchr(const void *s, int c, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
-void		*ft_free_arr(char **arr);
+void		ft_free_arr(char **arr);
 
 /*Lists*/
 t_list		*ft_lstnew(void *content);
--- src/libft/memory/ft_free_arr.c
+++ src/libft/memory/ft_free_arr.c
@@ -6,22 +6,24 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/08 16:35:49 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/08/29 14:15:09 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/09/05 15:12:59 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_free_arr(char **arr)
+void	ft_free_arr(char **arr)
 {
 	int	i;
 
 	i = 0;
+	if (!arr)
+		return ;
 	while (arr[i])
 	{
 		free(arr[i]);
 		i++;
 	}
 	free(arr);
-	return (NULL);
+	arr = NULL;
 }
